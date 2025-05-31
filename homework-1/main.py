b30d371117701fe96108123f72df78cc01a679d0
Aziat <167903432+Omurbek000@users.noreply.github.com>
2025-05-31 21:57:38 +0300

    Add files via upload

---
+++ homework-1/main.py
@@ -0,0 +1,62 @@
+# Тапшырма
+# 1. Өздүк маалыматтар
+# Өзүң жөнүндө маалыматты камтыган өзгөрмөлөрдү түз:
+
+# a = "Omurbek"
+# s = 29
+# d = 170
+
+# print("Atym =>,", a, "Jashym =>,", s, "Boyum =>,", d)
+
+# 2 .Студент экениңди көрсөтүүчү өзгөрмө (boolean: True же False)
+# Бул өзгөрмөлөрдү print() менен чыгарып, алардын түрүн (type()) көрсөт
+
+# boll_1 = 'true'
+
+# print(type(boll_1))
+
+# 2. Сандык операциялар
+# Төмөнкү операцияларды аткар:
+# Эки бүтүн (integer) санды алып, аларды кош, кемит, бөл, көбөйт, бөлүндүдөн калдык тап (%), даражага көтөр (**).
+# Эки бөлчөк (float) сан менен да ушундай эле операцияларды жаса.
+
+# a = 5
+# b = 5
+# s = a + b
+# f = a - b
+# g = a * b
+# h = a % b
+# j = a ** b
+
+# print ('Koshu =:', s , 'Kemitu =:', f , 'Koboitu =:', g , 'Kaldyk =:', h , 'Daraja =:' , j)
+
+# a = 5.7
+# b = 2.7
+# s = a + b
+# f = a - b
+# g = a * b
+# h = a % b
+# j = a ** b
+
+# print ('Koshu =:', s , 'Kemitu =:', f , 'Koboitu =:', g , 'Kaldyk =:', h , 'Daraja =:' , j)
+
+
+# 3. Математикалык эсептөөлөр
+# Формула: A = π * r² (айлананын аянтын тап)
+# Пи санын (3.14159) колдонуп, радиусу 7 болгон айлананын аянтын тап.
+
+# bul malymaty tushunboi kaldym
+
+
+# 4. Логикалык операциялар
+# Берилген өзгөрмөлөр үчүн and, or, not операцияларын колдонуп 10 мисал жаз
+
+# a = 5
+# b = 7
+# print(a and b)
+# print(a or b)
+# print(not a)
+# print(not b)
+
+# komentarii kaltyryp ketsenis tapshyrma boiuncha tuura kata jerlerin  chon rahmat ))
+
